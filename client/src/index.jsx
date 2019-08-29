7cd81c9eb2f4526051f07fcf63d3fdd94d43bd23
Ryan Zhong <ryanzhong@Ryans-MacBook-Pro.local>
2019-08-29 14:37:30 -0700

    added webfontloader

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -2,5 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import App from './components/App.jsx';
+import WebFont from 'webfontloader';
+
+
+WebFont.load({
+    google: {
+      families: ['Titillium Web:300,400,700', 'sans-serif']
+    }
+  });
 
 ReactDOM.render(<App /> , document.getElementById('app'));
